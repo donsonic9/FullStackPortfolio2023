2ef924a9b540c2e092494eea7b0f6ecb00625a82
donsonic9 <donsonic9@gmail.com>
2023-07-06 23:31:27 -0300

    MetaDataDinamyc

--- src/app/components/es/home-es/home-es.component.ts
+++ src/app/components/es/home-es/home-es.component.ts
@@ -11,12 +11,21 @@ export class HomeEsComponent implements OnInit {
 
   constructor( @Inject(DOCUMENT) private document: Document, private title:Title, private meta:Meta) {
     title.setTitle('Alexander Repollo | Desarrollador Web Full Stack');
-    meta.updateTag({ name:"description", content:"Bienvenido/a a mi portfolio! Soy Alex, y aquí vas a poder encontrar todos mis proyectos, información personal y mas. No dudes en contactarme!"});
-    meta.updateTag({ property:"og:image", content:"https://alexander-repollo.web.app/assets/images/AR-logo-alt-ogimage.png"});
+    
   }
 
   ngOnInit() {
     this.document.documentElement.lang = 'es';
+    this.updateMetaTags();
+  }
+
+  updateMetaTags(): void {
+    const newDescription = "Bienvenido/a a mi portfolio! Soy Alex, y aquí vas a poder encontrar todos mis proyectos, información personal y mas. No dudes en contactarme!";
+    const newImage = "https://alexander-repollo.web.app/assets/images/AR-logo-alt-ogimage.png";
+
+    this.meta.updateTag({ name:"description", content: newDescription});
+    this.meta.updateTag({ property:"og:description", content: newDescription});
+    this.meta.updateTag({ property:"og:image", content: newImage});
   }
 }
 
